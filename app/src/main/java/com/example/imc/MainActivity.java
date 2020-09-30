7d773d86676616db43eeb85491b0fd4282422776
joao.230887 <joaopaulo.ra04@gmail.com>
2020-09-30 17:38:45 -0300

    App IMC

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -3,63 +3,97 @@ package com.example.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.number.FormattedNumber;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText peso;
     private EditText altura;
-    private Button enviarButton;
+    private Button buttonEnvia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        peso.findViewById(R.id.pesoEditText);
-        altura.findViewById(R.id.alturaEditText);
-        enviarButton.findViewById(R.id.enviarButton);
+        peso = findViewById(R.id.pesoEditText);
+        altura = findViewById(R.id.alturaEditText);
+        buttonEnvia = findViewById(R.id.enviarButton);
 
-        enviarButton.setOnClickListener(new View.OnClickListener() {
+        buttonEnvia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (peso.getText().toString().isEmpty()){
+                    peso.setError("Informe o peso");
+                    return;
+                }if (altura.getText().toString().isEmpty()) {
+                    altura.setError("Informe a altura");
+                    return;
+                }
+
                 String pesoString = peso.getText().toString();
-                double peso = Double.parseDouble(pesoString);
+                double pesoD = Double.parseDouble(pesoString);
+                if (pesoD <= 0){
+                    peso.setError("Peso inválido");
+                    return;
+                }
+
                 String alturaString = altura.getText().toString();
-                double altura = Double.parseDouble(alturaString);
+                double alturaD = Double.parseDouble(alturaString);
+                if (alturaD <= 0){
+                    altura.setError("Altura inválida");
+                    return;
+                }
 
                 Intent intent = new Intent(MainActivity.this, ResultadoActivity.class);
-                intent.putExtra("Peso", peso);
-                intent.putExtra("Altura", altura);
-                //calculaIMC(peso,altura);
+                intent.putExtra("Peso", pesoString);
+                intent.putExtra("Altura", alturaString);
+
+                String imc = calculaIMC(pesoD, alturaD);
+                intent.putExtra("Resultado", imc);
+
+                Double resultadoIMC = imc(pesoD, alturaD);
+                String imcResultado = formatar(resultadoIMC);
+                intent.putExtra("IMC", imcResultado);
+
                 startActivity(intent);
             }
         });
     }
 
-//    private void calculaIMC(double peso, double altura) {
-//        String resultado;
-//        double imc = peso / (altura + altura);
-//        if (imc <= 18.5) {
-//            resultado = "Abaixo do peso";
-//        }
-//        if (imc >= 18.5 && imc <= 24.9) {
-//            resultado = "Peso Ideal";
-//        }
-//        if (imc >= 25 && imc <= 29.9) {
-//            resultado = "Levemente acima do peso";
-//        }
-//        if (imc >= 30 && imc <= 34.9) {
-//            resultado = "Obsidade grau I";
-//        }
-//        if (imc >= 35 && imc <= 39.9) {
-//            resultado = "Obsidade grau II(severa)";
-//        }
-//        if (imc >= 40) {
-//            resultado = "Obsidade grau III(mórbida)";
-//        }
-//    }
+    private String calculaIMC(double peso, double altura) {
+        double imc = peso / (altura * altura);
+        if (imc <= 18.5) {
+            return "Abaixo do peso";
+        }
+        if (imc >= 18.5 && imc <= 24.9) {
+            return "Peso Ideal";
+        }
+        if (imc >= 25 && imc <= 29.9) {
+            return "Levemente acima do peso";
+        }
+        if (imc >= 30 && imc <= 34.9) {
+            return "Obsidade grau I";
+        }
+        if (imc >= 35 && imc <= 39.9) {
+            return "Obsidade grau II(severa)";
+        }else{
+            return "Obsidade grau III(mórbida)";
+        }
+    }
+
+    private double imc(double peso, double altura){
+        return peso/(altura*altura);
+    }
+
+    private String formatar(Double numero){
+        DecimalFormat f = new DecimalFormat("0.##");
+        return f.format(numero);
+    }
 }
--- app/src/main/java/com/example/imc/ResultadoActivity.java
+++ app/src/main/java/com/example/imc/ResultadoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +12,8 @@ public class ResultadoActivity extends AppCompatActivity {
 
     private TextView pesoText;
     private TextView alturaText;
+    private TextView resultadoText;
+    private TextView imcText;
     private Button refazerButton;
 
     @Override
@@ -18,15 +21,27 @@ public class ResultadoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
-        pesoText.findViewById(R.id.pesoTextView);
-        alturaText.findViewById(R.id.alturaTextView);
-        refazerButton.findViewById(R.id.refazerButton);
+        pesoText = findViewById(R.id.pesoTextView);
+        alturaText = findViewById(R.id.alturaTextView);
+        resultadoText = findViewById(R.id.resultadoTextView);
+        imcText = findViewById(R.id.imcTextView);
+        refazerButton = findViewById(R.id.refazerButton);
 
         Intent intent = getIntent();
         String peso = intent.getStringExtra("Peso");
         String altura = intent.getStringExtra("Altura");
+        String resultado = intent.getStringExtra("Resultado");
+        String imc = intent.getStringExtra("IMC");
         pesoText.setText(peso);
         alturaText.setText(altura);
-
+        resultadoText.setText(resultado);
+        imcText.setText(imc);
+        refazerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultadoActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
